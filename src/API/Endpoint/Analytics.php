decc7b9b1a26959f40b90218589ba8bb05227a9c
artemiy1051 <artemiy1051@gmail.com>
2024-06-11 15:17:34 +0500

    287: Paid Storage

--- src/API/Endpoint/Analytics.php
+++ src/API/Endpoint/Analytics.php
@@ -10,6 +10,8 @@ use InvalidArgumentException;
 
 class Analytics extends AbstractEndpoint
 {
+    private const REQUEST_QTY = 12;
+    private const REQUEST_TIMEOUT = 5;
 
     /**
      * Получение статистики КТ за выбранный период,
@@ -236,6 +238,32 @@ class Analytics extends AbstractEndpoint
         return $this->postRequest('/api/v1/analytics/excise-report?dateFrom=' . $dateFrom->format('Y-m-d') . '&dateTo=' . $dateTo->format('Y-m-d'), [
             'countries' => $countries,
         ]);
+    }
+
+    /**
+     * Платное хранение
+     * 
+     * Будет выгружена информация о платном хранении
+     * 
+     * @param DateTime $dateFrom   Начало периода
+     * @param DateTime $dateTo     Конец периода
+     * 
+     * @return array { data: [object, object, ...] }
+     */
+    public function paidStorageReports(DateTime $dateFrom, DateTime $dateTo): array
+    {
+        $taskRequest = $this->getRequest('/api/v1/paid_storage', [
+            'dateFrom' => $dateFrom->format(DATE_RFC3339),
+            'dateTo' => $dateTo->format(DATE_RFC3339),
+        ]);
+        for ($i = 0; $i < self::REQUEST_QTY; $i++) { 
+            sleep(self::REQUEST_TIMEOUT);
+            $status = $this->getRequest("/api/v1/paid_storage/tasks/{$taskRequest->data->taskId}/status");
+            if ($status->data->status == 'done') {
+                break;
+            }
+        }
+        return $this->getRequest("/api/v1/paid_storage/tasks/{$taskRequest->data->taskId}/download");
     }
     
     private function getFromFilter(string $param, array $filter)
